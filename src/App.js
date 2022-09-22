cb3f2c34ab2d525fdd09cbb39374da427aa1b18b
iconicsudip <das366966@gmail.com>
2022-09-23 02:04:12 +0530

    Prototype

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import {useEffect,useState} from 'react';
+import Intro from "./components/Intro";
 
 function App() {
+  const [scrollVal,setScroll] = useState(0);
+  useEffect(() => {
+    const handleScroll = event => {
+      setScroll(window.scrollY);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header scrollVal = {scrollVal}/>
+      <Intro />
     </div>
   );
 }
---
+++ src/components/Intro.jsx
@@ -0,0 +1,72 @@
+import React from 'react'
+
+export default function Intro() {
+  return (
+    <div id="home" className="rn-slider-area">
+            <div className="slide slider-style-1">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-7">
+                            <div className="content">
+                                <div className="inner">
+                                    <span className="subtitle">Welcome to my world</span>
+                                    <h1 className="title">Hi, I'm <span>Jone Lee</span><br/>
+                                        <span className="header-caption" id="page-top">
+                                            <span className="cd-headline clip is-full-width">
+                                                <span>a </span>
+                                        <span className="cd-words-wrapper" >
+                                                    <b className="is-visible">Developer.</b>
+                                                    <b className="is-hidden">Professional Coder.</b>
+                                                    <b className="is-hidden">Developer.</b>
+                                                </span>
+                                        </span>
+                                        </span>
+                                    </h1>
+
+                                    <div>
+                                        <p className="description">I use animation as a third dimension by which to simplify
+                                            experiences and kuiding thro each and every interaction. I'm not adding
+                                            motion
+                                            just to spruce things up, but doing it in ways that.</p>
+                                    </div>
+                                </div>
+                                <div className="row">
+                                    <div className="col-lg-6 col-xl-6 col-md-6 col-sm-6 col-12">
+                                        <div className="social-share-inner-left">
+                                            <span className="title">find with me</span>
+                                            <ul className="social-share d-flex liststyle">
+                                                <li className="facebook"><a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-facebook"><path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path></svg></a>
+                                                </li>
+                                                <li className="instagram"><a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-instagram"><rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path><line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line></svg></a>
+                                                </li>
+                                                <li className="linkedin"><a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-linkedin"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path><rect x="2" y="9" width="4" height="12"></rect><circle cx="4" cy="4" r="2"></circle></svg></a>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                    <div className="col-lg-6 col-xl-6 col-md-6 col-sm-6 col-12 mt_mobile--30">
+                                        <div className="skill-share-inner">
+                                            <span className="title">best skill on</span>
+                                            <ul className="skill-share d-flex liststyle">
+                                                <li><img src="assets/images/icons/icons-01.png" alt="Icons Images"/></li>
+                                                <li><img src="assets/images/icons/icons-02.png" alt="Icons Images"/></li>
+                                                <li><img src="assets/images/icons/icons-03.png" alt="Icons Images"/></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col">
+                            <div className="thumbnail">
+                                <div className="inner">
+                                    <img src="assets/images/slider/banner-01.png" alt="Personal Portfolio Images"/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </div>
+  )
+}
