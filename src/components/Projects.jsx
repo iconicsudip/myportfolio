de8affb60fb0a39e2d47f627257323f06aa79fb1
iconicsudip <das366966@gmail.com>
2022-10-13 20:40:25 +0530

    real time search project added

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,20 +2,49 @@ import React from 'react'
 import { useState,useEffect } from 'react';
 import Projectcontainer from './Projectcontainer';
 import Modal from './Modal';
+import TextField from '@mui/material/TextField';
 
 export default function Projects() {
     const [projects,setProjects] = useState([]);
+    const [domain,setDomain] = useState([]);
+    const [search,setSearch] = useState("")
+    const final_dom = new Set();
     useEffect(()=>{
         (async ()=>{
             const response = await fetch('https://sheetpi.herokuapp.com/api/sheetdata/1FwUuZO2FrPdk-CTisPvp9AQTE3ogh778M0ilFb5JwAo');
             const raw = await response.json();
             var arr = []
+            var dom = []
             for(var i in raw){
                 arr.push(raw[i])
+                dom.push(raw[i]["Domain"])
             }
-            setProjects(arr)
+            if(search===""){
+                setProjects(arr)
+            }else{
+                var project = [];
+                project =arr.filter((p,ind)=>{
+                    return (p["Domain"].toLowerCase().includes(search.toLowerCase()) 
+                    || p["Description"].toLowerCase().includes(search.toLowerCase()) 
+                    || p["Heading"].toLowerCase().includes(search.toLowerCase())
+                    || p["Name"].toLowerCase().includes(search.toLowerCase()));
+                })
+                if(project.length===0){
+                    setProjects(null);
+                }else{
+                    setProjects(project)
+                }
+            }
+            setDomain([...dom])
         })();
-    },[projects])
+    },[domain,projects])
+    for(var i = 0;i<domain.length;i++){
+        final_dom.add(domain[i]);
+    }
+    const goSearch = (e)=>{
+        e.preventDefault();
+        setSearch(e.target.value);
+    }
     return (
         <div className="rn-portfolio-area rn-section-gap section-separator" id="portfolio">
             <div className="container">
@@ -24,16 +53,23 @@ export default function Projects() {
                         <div className="section-title text-center">
                             <span className="subtitle">Visit my portfolio and keep your testimonial</span>
                             <h2 className="title">My All Projects</h2>
+                            
+                            <TextField className='searchbar' value={search} onChange={goSearch} fullWidth label="Search Project" InputLabelProps={{style: { color: '#c4cfde'},}} id="fullWidth" style={{marginTop:"2rem"}}/>
+                            {/* {final_dom} */}
                         </div>
                     </div>
                 </div>
 
                 <div className="row row--25 mt--10 mt_md--10 mt_sm--10">
-                    {projects.map((project,id)=>(
+                    {projects===null?
+                    <div className="section-title text-center" style={{marginTop:"4rem"}}>
+                        <span className="subtitle">No project is like {search}</span>
+                    </div>
+                    :projects.map((project,id)=>(
                         <Projectcontainer project={project} id={id}/>
                     ))}
                 </div>
-                {projects.map((project,id)=>(
+                {projects===null?null:projects.map((project,id)=>(
                     <Modal project={project} id={id}/>
                 ))}
             </div>
